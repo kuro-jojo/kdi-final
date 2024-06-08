7fb5fdab136035ebca3ac03f66f8fbf8dc4f92ab
Jonathan ASSAMAGAN <80401029+kuro-jojo@users.noreply.github.com>
2024-06-08 12:36:14 +0000

    Initial commit

--- kdi-web/env.sh
+++ kdi-web/env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-encoded_string=$(echo -n "mongodb+srv://kdi:rnFiJaGZTHwKw8k0@kdi-cluster.mnpis8w.mongodb.net/?retryWrites=true&w=majority&appName=kdi-cluster&tls=true" | base64 -w 0)
+encoded_string=$(echo -n "http://kdi-webapp:8080" | base64 -w 0)
 echo "Encoded string: $encoded_string"
