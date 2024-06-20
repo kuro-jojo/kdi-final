e26559e68f57931dea98d5b12bd9ba656fbaac38
Jonathan ASSAMAGAN <80401029+kuro-jojo@users.noreply.github.com>
2024-06-20 19:49:11 +0000

    Initial commit

--- kdi-webapp/src/environments/set-env.ts
+++ kdi-webapp/src/environments/set-env.ts
@@ -12,7 +12,7 @@ const setEnv = () => {
     // `environment.ts` file structure
     const envConfigFile = `export const environment = {
     production: true,
-    apiUrl: '${process.env["KDI_WEBAPP_WEP_API_ENDPOINT"]}',
+    apiUrl: 'kdi-web.kuro08-dev.svc.cluster.local:8070/api/v1',
     clientId: '${process.env["KDI_WEBAPP_MSAL_CLIENT_ID"]}',
     redirectUri: '${process.env["KDI_WEBAPP_MSAL_REDIRECT_URI"]}',
     authority: '${process.env["KDI_WEBAPP_MSAL_AUTHORITY"]}',
